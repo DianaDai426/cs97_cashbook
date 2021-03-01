330b61b48d72c3f9f55cb1a9d8f826273b153a92
ashen0 <77872621+ashen0@users.noreply.github.com>
2021-03-01 00:01:23 -0800

    Created Token decoder that is used for "logged in" state. Has the functions to deal with logging in and logging out.

--- README.md
+++ README.md
@@ -4,10 +4,11 @@ This works on http://localhost:5000
 
 Then go into ./client and type the following commands:
 
-npm install -g yarn; 
+npm install -g yarn;
 yarn add @material-ui/core ;
 yarn add @material-ui/icons ;
 npm install semantic-ui-react semantic-ui-css;
+npm install jwt-decode ;
 yarn add semantic-ui-react;
 npm install react-router-dom --save ;
 npm install ;
--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,31 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import 'semantic-ui-css/semantic.min.css';
+import { Container } from 'semantic-ui-react';
+
+import {AuthProvider} from './context/auth';
+import AuthRoute from './util/AuthRoute';
+
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import MenuBar from './components/MenuBar'
-import 'semantic-ui-css/semantic.min.css'
-import { Container } from 'semantic-ui-react'
+import MenuBar from './components/MenuBar';
+
 
 export default function App() {
     return (
-    <Router>
-        <Container>
-          <MenuBar />
-          <Switch>
-            <Route exact path="/" component={Home}></Route>
-            <Route exact path='/login' component = {Login}></Route>
-            <Route exact path='/register' component = {Register}></Route>
-          </Switch>
-        </Container>
-    </Router>
+    <AuthProvider>
+      <Router>
+          <Container>
+            <MenuBar />
+            <Switch>
+              <Route exact path="/" component={Home}></Route>
+              <AuthRoute exact path='/login' component = {Login} />
+              <AuthRoute exact path='/register' component = {Register} />
+            </Switch>
+          </Container>
+      </Router>
+    </AuthProvider>
     )
 }
