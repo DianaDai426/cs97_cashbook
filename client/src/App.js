bd0a592e3bc7422373a520153383a97b8017727e
jonathan-c-lai <78984450+jonathan-c-lai@users.noreply.github.com>
2021-02-28 19:52:08 -0800

    Added most basic register page
    - basic register page
    - i believe it communicates with backend; not too sure, will need to check with others
    - using graphql-tag

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Register from './pages/Register';
 
 export default function App() {
     return (
@@ -9,6 +10,7 @@ export default function App() {
         <Switch>
           <Route exact path="/" component={Home}></Route>
           <Route exact path='/login' component = {Login}></Route>
+          <Route exact path='/register' component = {Register}></Route>
         </Switch>
     </Router>
     )
